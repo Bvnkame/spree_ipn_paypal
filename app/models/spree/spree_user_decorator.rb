729773ae87836aa874d5e0e2352fa566b6c179a5
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-19 21:14:54 +0700

    U[

--- app/models/spree/spree_user_decorator.rb
+++ app/models/spree/spree_user_decorator.rb
@@ -1,5 +1,5 @@
 Spree::User.class_eval do 
 
-	has_many :user_accounts, :class_name => "Prepaid::UserAccount", foreign_key: 'user_id'
+	has_one :user_account, :class_name => "Prepaid::UserAccount", foreign_key: 'user_id'
 
 end
